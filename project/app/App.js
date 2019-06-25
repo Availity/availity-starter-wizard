94d7448831619756b935f2b478d7cdb9e197aa37
Kyle Gray <Kyle.Gray@availity.com>
2019-06-25 09:38:16 -0400

    refactor: fixed undefined issue

--- project/app/App.js
+++ project/app/App.js
@@ -1,43 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter, Route } from 'react-router-dom';
-import { Container, Card } from 'reactstrap';
+import { Container, Row, Col, Alert, Card } from 'reactstrap';
+import { AvForm, AvField } from 'availity-reactstrap-validation';
+import { AvSelectField } from '@availity/reactstrap-validation-select';
+import { AvOrganizationSelect, AvProviderSelect } from '@availity/reactstrap-validation-select/resources';
+
 import Spaces from '@availity/spaces';
 import PageHeader from '@availity/page-header';
-import Wizard, { WizardStep, WizardStepTitle, WizardStepBadge } from '@availity/step-wizard';
 import qs from 'query-string';
 import get from 'lodash.get';
-import { Search, Results } from './areas';
+// import { Search, Results } from './areas';
 
-const App = ({ location, history }) => {
+const App = ({ location }) => {
+  const [customerId, setCustomerId] = useState(null);
   const spaceId = get(qs.parse(location.search), 'spaceId');
 
   return (
     <Spaces spaceIds={[spaceId]} clientId="test">
-      <Container data-testid="app-container">
-        <PageHeader appName="Claims Status Listing" spaceId={spaceId} feedback/>
-        <Card body>
-          <Wizard bar>
-            <WizardStep
-              clickable={location.pathname !== '/'}
-              complete={location.pathname === '/search'}
-              active={location.pathname === '/'}
-              onClick={() => history.push(`/?spaceId=${spaceId}`)}
-            >
-              <WizardStepBadge>1</WizardStepBadge>
+      <Container data-testid="app-container" fluid>
+        <PageHeader appName="Provider Scorecard" spaceId={spaceId} feedback />
+        <AvForm>
+          <Row>
+            <Col xs={4}>
+              <AvSelectField
+                label={
+                  <label>
+                    <span className="text-primary">Step 1 -</span> Choose Product
+                  </label>
+                }
+                options={[{ label: 'Medicare', value: 'medicare' }]}
+                name="product"
+                placeholder="Choose Product"
+              />
+            </Col>
 
-              <WizardStepTitle>Search Claims</WizardStepTitle>
-            </WizardStep>
-            <WizardStep
-              active={location.pathname === '/search'}
-            >
-              <WizardStepBadge>2</WizardStepBadge>
-              <WizardStepTitle>Search Results</WizardStepTitle>
-            </WizardStep>
-          </Wizard>
-          <Route exact path="/" component={Search} />
-          <Route path="/search" component={Results} />
-        </Card>
+            <Col xs={4}>
+              <AvOrganizationSelect
+                label={
+                  <label>
+                    <span className="text-primary">Step 2 -</span> Choose Organization
+                  </label>
+                }
+                onChange={({ customerId }) => setCustomerId(customerId)}
+                name="organization"
+                placeholder="Choose Organization"
+              />
+            </Col>
+
+            <Col xs={4}>
+              <AvProviderSelect
+                label={
+                  <label>
+                    <span className="text-primary">Step 3 -</span> Choose Group
+                  </label>
+                }
+                customerId={customerId}
+                name="provider"
+                type="text"
+                placeholder="Choose Group"
+              />
+            </Col>
+          </Row>
+          <Card body>
+            <Alert color="primary">You have potentially earned $XX,XXX.XX out of a maximum of</Alert>
+          </Card>
+        </AvForm>
       </Container>
     </Spaces>
   );
@@ -45,7 +73,7 @@ const App = ({ location, history }) => {
 
 App.propTypes = {
   location: PropTypes.object,
-  history: PropTypes.object
+  history: PropTypes.object,
 };
 
 export default withRouter(App);
